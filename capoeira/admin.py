da08a724ed0dfd50c04202b39afee723c237a02a
BrianeBianca <brianebianca@gmail.com>
2015-11-21 23:29:58 -0200

    Admin
    Configurado o CRUD dos objetos no admin.

--- capoeira/admin.py
+++ capoeira/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Endereco
+from .models import Grupo
+from .models import Pessoa
+from .models import Aluno
+from .models import Professor
+from .models import Turma
+from .models import Corda
 
-# Register your models here.
+admin.site.register(Endereco)
+admin.site.register(Grupo)
+admin.site.register(Pessoa)
+admin.site.register(Aluno)
+admin.site.register(Professor)
+admin.site.register(Turma)
+admin.site.register(Corda)
+
+
